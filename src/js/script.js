932974365aba0c7c66a1e964464f25f76055e6c7
x1zy <nikitagusev672@gmail.com>
2023-07-31 22:36:31 +0300

    feat: add player switch

--- src/js/script.js
+++ src/js/script.js
@@ -9,13 +9,18 @@ const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 
 // Game initial conditions
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
+const totalScores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
 // Roll the dice
 btnRoll.addEventListener('click', function () {
     // Generate a random number
@@ -28,6 +33,12 @@ btnRoll.addEventListener('click', function () {
     // If the number is 1, switch to the next player
     if (diceNumber !== 1) {
         currentScore += diceNumber;
-        current0Element.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+        currentScore = 0;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0Element.classList.toggle('player--active');
+        player1Element.classList.toggle('player--active');
     }
-})
+});
